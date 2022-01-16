0b789c3610f135a954ed221af264d26c516d3eda
Sebastian <sese.menne@web.de>
2022-01-16 01:31:33 +0100

    Added documentation

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,15 @@
+/*
+Primitive Types--
+Integers: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128 (number of bits they take in memory)
+Floats: f32, f64
+Boolean (bool)
+Characters (char)
+Tuples
+Arrays
+*/
+
+// Rust is a statically typed language, which means that it must know the types of all variables at compile time,
+// however, the compiler can usually infer what type we want to use based on the value and how we use it.
 
 
 pub fn run(){
@@ -20,6 +32,7 @@ pub fn run(){
     //get boolean from expression
     let is_greater = 10>5;
 
+    //char (also unicode)
     let a1 = 'a';
     let face = '\u{1f600}';
 
