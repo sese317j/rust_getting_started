cf458cffbb81085b23e56a6d4a4ce231b4f389f8
Sebastian <sese.menne@web.de>
2022-01-21 00:57:34 +0100

    Running without args now doesnt lead to a crash

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,13 @@ use std::env;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    let command = args[1].clone();
+
+    let mut command:String = String::from("");
+    if args.len() > 1  {
+        command = args[1].clone();
+    }
+
+
     let name = "Brad";
     let status = "100%";
 
